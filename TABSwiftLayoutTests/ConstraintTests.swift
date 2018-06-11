2446d77dbb0b5156e80bc303ad4cfae4c461d268
Jonathan Bailey <jonathan.bailey13@imperial.ac.uk>
2018-06-11 18:07:23 +0530

    Match project formatting

--- TABSwiftLayoutTests/ConstraintTests.swift
+++ TABSwiftLayoutTests/ConstraintTests.swift
@@ -9,25 +9,25 @@
 import XCTest
 
 class ConstraintTests: XCTestCase {
-	
-	private func simpleConstraint(view: UIView = UIView(), attribute: NSLayoutAttribute) -> NSLayoutConstraint {
-		return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: UIView(), attribute: attribute, multiplier: 1, constant: 0)
-	}
-	
-    func testConstraintTrait() {
-        XCTAssertEqual(simpleConstraint(attribute: .top).trait, .TopMargin)
-        XCTAssertEqual(simpleConstraint(attribute: .left).trait, .LeftMargin)
-        XCTAssertEqual(simpleConstraint(attribute: .leading).trait, .LeftMargin)
-        XCTAssertEqual(simpleConstraint(attribute: .right).trait, .RightMargin)
-        XCTAssertEqual(simpleConstraint(attribute: .trailing).trait, .RightMargin)
-        XCTAssertEqual(simpleConstraint(attribute: .bottom).trait, .BottomMargin)
-        
-        XCTAssertEqual(simpleConstraint(attribute: .centerX).trait, .HorizontalAlignment)
-        XCTAssertEqual(simpleConstraint(attribute: .centerY).trait, .VerticalAlignment)
-        
-        XCTAssertEqual(simpleConstraint(attribute: .width).trait, .HorizontalSizing)
-        XCTAssertEqual(simpleConstraint(attribute: .height).trait, .VerticalSizing)
-        
-        XCTAssertEqual(simpleConstraint(attribute: .lastBaseline).trait, .None)
-    }
+  
+  private func simpleConstraint(view: UIView = UIView(), attribute: NSLayoutAttribute) -> NSLayoutConstraint {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: UIView(), attribute: attribute, multiplier: 1, constant: 0)
+  }
+  
+  func testConstraintTrait() {
+    XCTAssertEqual(simpleConstraint(attribute: .top).trait, .TopMargin)
+    XCTAssertEqual(simpleConstraint(attribute: .left).trait, .LeftMargin)
+    XCTAssertEqual(simpleConstraint(attribute: .leading).trait, .LeftMargin)
+    XCTAssertEqual(simpleConstraint(attribute: .right).trait, .RightMargin)
+    XCTAssertEqual(simpleConstraint(attribute: .trailing).trait, .RightMargin)
+    XCTAssertEqual(simpleConstraint(attribute: .bottom).trait, .BottomMargin)
+    
+    XCTAssertEqual(simpleConstraint(attribute: .centerX).trait, .HorizontalAlignment)
+    XCTAssertEqual(simpleConstraint(attribute: .centerY).trait, .VerticalAlignment)
+    
+    XCTAssertEqual(simpleConstraint(attribute: .width).trait, .HorizontalSizing)
+    XCTAssertEqual(simpleConstraint(attribute: .height).trait, .VerticalSizing)
+    
+    XCTAssertEqual(simpleConstraint(attribute: .lastBaseline).trait, .None)
+  }
 }
